3686b0958dc4eff2e80bb27f0706d7ea401cdc2e
Volha Melayok <neiwra@gmail.com>
2024-02-20 15:02:39 +0100

    feat: add displaying of recipe ingredients and steps

--- src/app/models/mock/recipes.mock.ts
+++ src/app/models/mock/recipes.mock.ts
@@ -4,4 +4,13 @@ export const RecipesMock: IRecipe[] = [
   { id: 1, name: 'first', ingredients: [{ name: 'sugar', count: 100, measure: 'g', }], steps: [{description: 'Make smth'}] },
   { id: 2, name: 'second', ingredients: [{ name: 'milk', count: 200, measure: 'ml', }], steps: [{description: 'Make smth else'}] },
   { id: 3, name: 'third', ingredients: [{ name: 'lemon', count: 1, measure: 'pcs', }], steps: [{description: 'Make totally smth else'}] },
+  { id: 4, name: 'fourth', ingredients: [
+    { name: 'lemon', count: 1, measure: 'pcs', },
+      { name: 'lemon', count: 1, measure: 'pcs', }
+    ],
+    steps: [
+    {description: 'Make totally smth else'}
+    ] },
 ];
+
+
