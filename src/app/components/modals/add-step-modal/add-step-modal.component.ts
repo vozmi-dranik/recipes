0d5a5dc0ecbe4491bbe8a18354f7c219ca0177c7
MDz1985 <m_d@tut.by>
2024-05-11 19:36:17 +0200

    feat: add necessary fields to add-step-form

--- src/app/components/modals/add-step-modal/add-step-modal.component.ts
+++ src/app/components/modals/add-step-modal/add-step-modal.component.ts
@@ -1,6 +1,14 @@
 import { Component, inject } from '@angular/core';
 import { MatDialogActions, MatDialogClose, MatDialogContent, MatDialogRef, MatDialogTitle } from '@angular/material/dialog';
 import { MatButton } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { DropzoneMaterialModule } from '@ngx-dropzone/material';
+import { MatIconModule } from '@angular/material/icon';
+import { DropzoneCdkModule } from '@ngx-dropzone/cdk';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatInput } from '@angular/material/input';
+import { CdkTextareaAutosize } from '@angular/cdk/text-field';
 
 @Component({
   selector: 'app-add-step-modal',
@@ -10,11 +18,24 @@ import { MatButton } from '@angular/material/button';
     MatDialogActions,
     MatDialogTitle,
     MatButton,
-    MatDialogClose
+    MatDialogClose,
+    MatFormFieldModule,
+    DropzoneCdkModule,
+    DropzoneMaterialModule,
+    MatIconModule,
+    MatChipsModule,
+    ReactiveFormsModule,
+    MatInput,
+    CdkTextareaAutosize
   ],
   templateUrl: './add-step-modal.component.html',
   styleUrl: './add-step-modal.component.scss'
 })
 export class AddStepModalComponent {
   dialogRef = inject<MatDialogRef<AddStepModalComponent>>(MatDialogRef);
+  fileCtrl = new FormControl();
+
+  clear() {
+    this.fileCtrl.setValue(null);
+  }
 }
