a4fd101e1075907b6ca23f671a9bab8ff4bb9668
MDz1985 <m_d@tut.by>
2024-05-31 11:23:32 +0200

    fix: use .env instead of environment.ts

--- codegen.ts
+++ codegen.ts
@@ -1,8 +1,14 @@
 import type { CodegenConfig } from '@graphql-codegen/cli';
-import { env } from './src/environments/environment';
+// import { env } from './src/environments/environment';
+
+
+import * as dotenv from 'dotenv';
+import * as process from 'node:process';
+dotenv.config();
 
 const config: CodegenConfig = {
-  schema: env.apiUrl,
+  schema: process.env?.['API_URL'] || '',
+  // schema: env.apiUrl,
   documents: './src/**/*.graphql',
   generates: {
     './graphql/generated.ts': {
--- src/app/graphql.provider.ts
+++ src/app/graphql.provider.ts
@@ -2,9 +2,15 @@ import { Apollo, APOLLO_OPTIONS } from 'apollo-angular';
 import { HttpLink } from 'apollo-angular/http';
 import { ApplicationConfig, inject } from '@angular/core';
 import { ApolloClientOptions, InMemoryCache } from '@apollo/client/core';
-import { env } from 'src/environments/environment';
+import * as process from 'node:process';
+// import { env } from 'src/environments/environment';
+// todo: replace with env from environment.ts later (problems with workflows) or use the env server or store to provide variables or use
+//  custom webpack
+import * as dotenv from 'dotenv';
+dotenv.config();
 
-const uri = env.apiUrl;
+// const uri = env.apiUrl;
+const uri = process.env['API_URL'] || '';
 export function apolloOptionsFactory(): ApolloClientOptions<any> {
   const httpLink = inject(HttpLink);
   return {
