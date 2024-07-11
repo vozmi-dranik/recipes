a3dac1ec271f28703b5798d9abbdf0e8bd58d42c
MDz1985 <m_d@tut.by>
2024-07-11 15:16:43 +0200

    feat: add basic layout to the recipe page

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,48 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
+    colors: {
+      primary: '#0f172a',
+      secondary: '#64748b',
+      tertiary: '#f1f5f9',
+      surface: '#f8fafc',
+      onSurface: '#000000',
+    },
+    fontSize: {
+      'body-medium': ['16px', {
+        lineHeight: '100%',
+        fontWeight: '500',
+        letterSpacing: '1',
+      }],
+      'label-medium': ['16px', {
+        lineHeight: '100%',
+        fontWeight: '500',
+        letterSpacing: '1',
+      }],
+      'headline-small': ['24px', {
+        lineHeight: '100%',
+        fontWeight: '500',
+        letterSpacing: '1',
+      }],
+      'headline-medium': ['32px', {
+        lineHeight: '100%',
+        fontWeight: '500',
+        letterSpacing: '1',
+      }],
+      'headline-large': ['40px', {
+        lineHeight: '100%',
+        fontWeight: '500',
+        letterSpacing: '1',
+      }],
+    },
+    screens: {
+      'xs': '0px',
+      's': '600px',
+      'desktop': '800px',
+      'm': '905px',
+      'l': '1240px',
+      'xl': '1440px',
+    },
     extend: {},
   },
   plugins: [],
